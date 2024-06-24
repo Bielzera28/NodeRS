5fd5f434b733445fc54d37d8201bbef7887850b1
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-06-24 19:35:44 -0300

    route params

--- src/server.js
+++ src/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 
 const app = express()
 
-app.get("/", (request, response) => {
-  response.send("Hello, world!")
+app.get("/:id/:user", (request, response) => {
+  const {id, user} = request.params;
+  response.send(`Id da mensagem: ${id}. Para o usuário ${user}`);
 });
 
 const PORT = 3000;
