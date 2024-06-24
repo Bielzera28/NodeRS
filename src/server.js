3649d7456ef525838fd9514e5fa252b09f7f5f9d
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-06-24 19:44:36 -0300

    query params

--- src/server.js
+++ src/server.js
@@ -7,5 +7,10 @@ app.get("/:id/:user", (request, response) => {
   response.send(`Id da mensagem: ${id}. Para o usuário ${user}`);
 });
 
+app.get("/users", (request, response) => {
+  const {page, limit} = request.query;
+  response.send(`Página: ${page}. Mostrar ${limit}`);
+})
+
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
