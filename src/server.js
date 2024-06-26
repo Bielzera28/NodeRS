53df61b4fbcd1fdcb6c36df92c1668f9b2656c7a
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-06-25 21:25:02 -0300

    add body params

--- src/server.js
+++ src/server.js
@@ -1,15 +1,17 @@
 const express = require("express");
 
 const app = express()
+app.use(express.json());
 
+/* METODO GET - ROUTE PARAMS
 app.get("/:id/:user", (request, response) => {
   const {id, user} = request.params;
   response.send(`Id da mensagem: ${id}. Nome do usuário ${user}`);
-});
+}); */
 
-app.get("/users", (request, response) => {
-  const {page, limit} = request.query;
-  response.send(`Página: ${page}. Mostrar ${limit}`);
+app.post("/users", (request, response) => {
+  const {name, email, age} = request.body;
+  response.json({name, email, age});
 })
 
 const PORT = 3000;
