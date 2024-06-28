423b8da61b0ddfd922595924040110de189d10ad
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-06-27 21:40:34 -0300

    admin middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,12 +3,22 @@ const usersRoutes = Router()
 const UsersController = require("../controllers/UsersController")
 const usersController = new UsersController()
 
+function myMiddleware(request, response, next) {
+  console.log("Você passou pelo Middleware!")
+  
+  if(!request.body.isAdmin){
+    return response.json("user unauthorized")
+  }
+
+  next()
+} 
+
 /* METODO GET - ROUTE PARAMS
 app.get("/:id/:user", (request, response) => {
   const {id, user} = request.params;
   response.send(`Id da mensagem: ${id}. Nome do usuário ${user}`);
 }); */
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = usersRoutes;
