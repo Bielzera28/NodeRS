5b0327516d63eea2226ff31c2bd384c5f7bccedd
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-06-27 22:15:16 -0300

    handling exceptions

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
   create(request,response) {
     const {name, email, age} = request.body;
+
+    if(!name){
+      throw new AppError ("Nome é obrigatório")
+    }
+
     response.status(201).json({name, email, age});
   }
 
