8a7960f0c3aec74ef2186e8e38fb21819ffb2709
Gabriel Aguiar <aguiar.desenvolvedor@gmail.com>
2024-07-01 19:10:02 -0300

    select in controller

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,7 +1,8 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-  create(request,response) {
+  async create(request,response) {
     const {name, email, age} = request.body;
 
     if(!name){
@@ -9,6 +10,13 @@ class UsersController {
     }
 
     response.status(201).json({name, email, age});
+    const database = await sqliteConnection();
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+
+    if (checkUserExists) {
+      throw new AppError("Este e-mail já está em uso.")
+    }
+    return response.status(201).json();
   }
 
 }
